b0155b08dee144e07f0b0db071de7974d3eab99e
sonoranora <nbiteniece@gmail.com>
2018-06-14 14:34:01 +0300

    initial

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Aizsardzības Ministrija</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/stylish-portfolio.min.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+    <!-- Services -->
+    <section class="content-section bg-primary text-white text-center" id="services">
+      <div class="container">
+        <div class="content-section-heading">
+<!--           <h3 class="text-secondary mb-0">Services</h3>
+          <h2 class="mb-5">What We Offer</h2> -->
+        </div>
+        <div class="row">
+          <div class="col-sm-12 col-lg-12 col-md-12">
+            <div class="row">
+            <div class="col-sm-6 col-lg-6 col-md-6">
+              <h2 style="text-align:left;">
+                <strong>14:25:30 Ceturtdiena, 14. jūnijs Saiva, Saivis</strong>
+              </h2>
+              <ul style="list-style-type: circle;">
+                <li><h5>Lorem ipsum dolor sit amet, consect</h5></li>
+                <li><h5>Lorem ipsum dolor sit amet, consect</h5></li>
+                <li><h5>Lorem ipsum dolor sit amet, consect</h5></li>
+                <li><h5>Lorem ipsum dolor sit amet, consect</h5></li>
+              </ul>
+            </div>
+            <div class="col-sm-6 col-lg-6 col-md-6">
+              <iframe align="right" src="http://flickrslidr.com/slideshow/view.php?g=bpKeek" frameBorder="0" width="100%" height="350" scrolling="no"></iframe><br/>
+            </div>
+          </div>
+          </div>
+          <div class="col-lg-6 col-md-">
+            <h4 style="text-align:left;">
+              <strong>Aktualitātes</strong>
+            </h4>
+            <p  class="text-faded mb-0"  style="text-align:left;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea comm..</p>
+            <hr></hr>
+            <h4 style="text-align:left;">
+              <strong>Paziņojumi</strong>
+            </h4>
+            <p  class="text-faded mb-0"  style="text-align:left;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea comm..</p>
+          </div>
+          <div class="col-lg-6 col-md-">
+            <h4></h4>
+            <a class="twitter-timeline" href="https://twitter.com/AizsardzibasMin?ref_src=twsrc%5Etfw">Tweets by AizsardzibasMin</a>
+            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/stylish-portfolio.min.js"></script>
+
+  </body>
+
+</html>
