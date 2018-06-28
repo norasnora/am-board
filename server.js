4f2b8db65aac2aa38f8df7d4bca2985650570d6b
sonoranora <nbiteniece@gmail.com>
2018-06-29 01:12:45 +0300

    iframe55

--- server.js
+++ server.js
@@ -19,15 +19,26 @@ var Flickr = require("flickrapi"),
 server.timeout = 1000 * 60 * 10; // 10 minutes
 
 // Use middleware to set the default Content-Type
-app.use(function (req, res, next) {
-    res.header('Content-Type', 'application/json');
-    next();
+// app.use(function (req, res, next) {
+//     res.header('Content-Type', 'application/json');
+//     next();
+// });
+
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Content-Type', 'application/json');
+  next();
 });
+
 
 app.get('/api/photos', (req, resp) => {
 	var cdata = [];
 	var str="";
-	var url = 'https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&user_id=52010134@N05&api_key=d9ad2d315dd33f36080a375d112ebbc9&format=json&nojsoncallback=1'
+	var str2 = "";
+	var url = 'https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&user_id=52010134@N05&api_key=d9ad2d315dd33f36080a375d112ebbc9&extras=url_o&format=json&nojsoncallback=1'
+	//var url2 = 'https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&photo_id=';
 	https.get(url, function(res) {
      if (res.statusCode >= 200 && res.statusCode < 400) {
      	res.on('data',function(data_){
@@ -38,12 +49,13 @@ app.get('/api/photos', (req, resp) => {
      	res.on('end',()=>{
      		try{
      			const data = JSON.parse(str);
+     			console.log(JSON.stringify(data));
      			arr = data['photos']['photo'];
-     			for(var i in arr){
-     				//console.log(i);
-     				cdata.push('https://www.flickr.com/photos/latvijas_armija/'+arr[i]['id']);
+     			for(var i in arr){     				
+     				cdata.push(arr[i]['url_o']);
      			}
-     			resp.send(JSON.stringify(cdata));
+     				resp.send(JSON.stringify(cdata));
+     			
 
      		}catch(er){
      			console.log(er);
